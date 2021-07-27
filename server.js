04149b4d78f0e311c87025a443ea2db7e44bd940
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2021-07-27 23:19:37 +0530

    add teams page

--- server.js
+++ server.js
@@ -7,7 +7,19 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", {
+        Result:"",
+        color: "white",
+    });
+})
+app.post("/", (req, res) => {
+    res.render("index", {
+        Result:"This is a fake news",
+        color: "red"
+    });
+})
+app.get("/teams", (req,res)=>{
+    res.render("teams");
 })
 http.listen(PORT, () => {
     console.log(`listening on ${PORT}`);
