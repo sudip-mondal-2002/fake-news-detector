4458cf0e4424411d55ef851f97e2a82bbf1d35d9
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2021-07-29 14:00:06 +0530

    added favicons

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const http = require('http').Server(app);
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static(__dirname+'/public'));
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
